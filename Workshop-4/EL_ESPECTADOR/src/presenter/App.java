ba72a9ac28198c0f545ac5e949205efbdced7ad5
SebsBarr <luiscorrbarrseb@gmail.com>
2025-06-27 09:58:53 -0500

    Add files via upload

---
+++ Workshop-4/EL_ESPECTADOR/src/presenter/App.java
@@ -0,0 +1,10 @@
+package presenter;
+import gui.MainFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("App :D");
+        MainFrame testGUI = new MainFrame();
+        
+    }
+}
